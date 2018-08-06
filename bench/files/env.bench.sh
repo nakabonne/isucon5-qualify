3df2bb3785da5c7f0cb18dd95c09a9133ce2566a
root <root@isucon5q-1.c.isucon-210313.internal>
2018-08-06 13:02:28 +0900

    一応benchまで走った

---
+++ bench/files/env.bench.sh
@@ -0,0 +1,7 @@
+PATH=/home/isucon/.local/ruby/bin:/usr/local/bin:/usr/local/bin:/usr/bin:/bin
+LANG=ja_JP.UTF-8
+RACK_ENV=production
+_JAVA_OPTIONS="-Dfile.encoding=UTF8 -Duser.timezone=JST"
+JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8
+JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export PATH LANG RACK_ENV _JAVA_OPTIONS JAVA_TOOL_OPTIONS JAVA_HOME
